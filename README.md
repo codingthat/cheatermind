6991203673a4d71f0609a17c90fb73163e64e598
IBwWG <inthebeginningwasthewordgame@gmail.com>
2017-03-14 09:01:03 +0100

    Eliminate question answered by analysis (and Knuth, directly.)

--- README.md
+++ README.md
@@ -46,4 +46,3 @@ npm test
 1. What's the minimum number of moves for a given slot + colour configuration in this worst-case scenario?  Does the current implementation always produce it?
 1. [Is it possible to come up with a formula for this?](http://math.stackexchange.com/questions/2182321/the-best-worst-case-scenario-in-mastermind)
 1. How can the AI be made more memory-efficient, so as to scale better?
-1. Is the tiebreaker case in the current AI implementation ever possible?  I.e., does there exist a scenario in which the AI would have more than one equally "most unlucky" grade to give the player?
--- tests/guess.js
+++ tests/guess.js
@@ -5,33 +5,34 @@ var o = require("../node_modules/mithril/ospec")
 var a = require("../src/js/AI")
 
 o.spec("guess", function() { // WARNING: guess mutates AI.possibleCodes (for efficiency) so an .init() call is needed to reset it
-    o("example 1", function() {
-        a.init(4, 6);
-        o(a.guess([0,0,0,0]).grade).deepEquals([0, 0, 4]);
-        o(a.guess([1,1,1,1]).grade).deepEquals([1, 0, 3]);
-        o(a.guess([2,3,4,5]).grade).deepEquals([0, 2, 2]);
-        o(a.guess([1,3,4,5]).grade).deepEquals([0, 3, 1]);
-        o(a.guess([1,1,4,5]).grade).deepEquals([0, 2, 2]);
-        o(a.guess([2,3,1,1]).grade).deepEquals([1, 1, 2]);
-        o(a.guess([0,0,1,1]).grade).deepEquals([1, 0, 3]);
-        o(a.guess([0,0,0,1]).grade).deepEquals([1, 0, 3]);
-        o(a.guess([3,3,3,1]).grade).deepEquals([2, 0, 2]);
-        o(a.guess([0,0,3,1]).grade).deepEquals([2, 0, 2]);
-        o(a.guess([4,4,3,1]).grade).deepEquals([2, 0, 2]);
-        o(a.guess([5,5,3,1]).grade).deepEquals([4, 0, 0]);
-    })
-    o("example 2", function() {
-        a.init(7,7);
-        o(a.guess([0,1,2,3,4,5,6]).grade).deepEquals([1, 4, 2]);
-        o(a.guess([4,3,2,1,0,0,0]).grade).deepEquals([1, 3, 3]);
-        o(a.guess([5,1,3,2,1,1,0]).grade).deepEquals([1, 3, 3]);
-        o(a.guess([6,5,0,3,2,2,0]).grade).deepEquals([0, 3, 4]);
-        o(a.guess([3,1,6,4,3,0,1]).grade).deepEquals([1, 4, 2]);
-        o(a.guess([2,3,4,6,4,1,1]).grade).deepEquals([2, 4, 1]);
-        o(a.guess([4,0,1,2,4,6,1]).grade).deepEquals([0, 7, 0]);
-        o(a.guess([1,1,4,6,0,4,2]).grade).deepEquals([3, 4, 0]);
-        o(a.guess([1,2,4,4,0,1,6]).grade).deepEquals([7, 0, 0]);
-    })
+    // these are commented out, pending analysis of what canonical grading should be:
+    // o("example 1", function() {
+    //     a.init(4, 6);
+    //     o(a.guess([0,0,0,0]).grade).deepEquals([0, 0, 4]);
+    //     o(a.guess([1,1,1,1]).grade).deepEquals([1, 0, 3]);
+    //     o(a.guess([2,3,4,5]).grade).deepEquals([0, 2, 2]);
+    //     o(a.guess([1,3,4,5]).grade).deepEquals([0, 3, 1]);
+    //     o(a.guess([1,1,4,5]).grade).deepEquals([0, 2, 2]);
+    //     o(a.guess([2,3,1,1]).grade).deepEquals([1, 1, 2]);
+    //     o(a.guess([0,0,1,1]).grade).deepEquals([1, 0, 3]);
+    //     o(a.guess([0,0,0,1]).grade).deepEquals([1, 0, 3]);
+    //     o(a.guess([3,3,3,1]).grade).deepEquals([2, 0, 2]);
+    //     o(a.guess([0,0,3,1]).grade).deepEquals([2, 0, 2]);
+    //     o(a.guess([4,4,3,1]).grade).deepEquals([2, 0, 2]);
+    //     o(a.guess([5,5,3,1]).grade).deepEquals([4, 0, 0]);
+    // })
+    // o("example 2", function() {
+    //     a.init(7,7);
+    //     o(a.guess([0,1,2,3,4,5,6]).grade).deepEquals([1, 4, 2]);
+    //     o(a.guess([4,3,2,1,0,0,0]).grade).deepEquals([1, 3, 3]);
+    //     o(a.guess([5,1,3,2,1,1,0]).grade).deepEquals([1, 3, 3]);
+    //     o(a.guess([6,5,0,3,2,2,0]).grade).deepEquals([0, 3, 4]);
+    //     o(a.guess([3,1,6,4,3,0,1]).grade).deepEquals([1, 4, 2]);
+    //     o(a.guess([2,3,4,6,4,1,1]).grade).deepEquals([2, 4, 1]);
+    //     o(a.guess([4,0,1,2,4,6,1]).grade).deepEquals([0, 7, 0]);
+    //     o(a.guess([1,1,4,6,0,4,2]).grade).deepEquals([3, 4, 0]);
+    //     o(a.guess([1,2,4,4,0,1,6]).grade).deepEquals([7, 0, 0]);
+    // })
     o("tiny test", function() {
         a.init(1, 2);
         o(a.guess([1]).grade).deepEquals([0, 0, 1]);
